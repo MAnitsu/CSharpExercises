4bbd9040143c322555e42d8b91bde27f37e33a64
MAnitsu <nitu.alexandru96@gmail.com>
2024-09-26 15:13:43 +0300

    Add project files.

---
+++ CSharpExercises/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter a number.
+            //The number should be between 1 to 10.
+            //If the user enters a valid number, display "Valid" on the console.
+            //Otherwise, display "Invalid".  
+
+            while (true)
+            {
+                Console.WriteLine("Insert a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number > 0 && number <= 10)
+                {
+                    Console.WriteLine("Valid");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                    continue;
+                }
+            }
+        }
+    }
+}
