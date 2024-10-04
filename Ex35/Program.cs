d4da90d079fe6318c3efbf62669bd1bb7457ab5c
MAnitsu <darrek96@gmail.com>
2024-10-04 20:55:31 +0300

    solved bug for ex 35

--- Ex35/Program.cs
+++ Ex35/Program.cs
@@ -9,12 +9,6 @@
             // display "Invalid List" and ask the user to re-try;
             // otherwise, display the 3 smallest numbers in the list.
 
-            // create a list where the input numbers will be stored
-            var myNums = new List<int>();
-
-            // create an empty list for the 3 smallest values to be stored in
-            var smallestNums = new List<int>();
-
             // creates a loop so the user will be asked to retry if the list is invalid
             while (true)
             {
@@ -23,51 +17,33 @@
                 string input = Console.ReadLine();
 
                 // checks if the list is empty
-                if (!String.IsNullOrEmpty(input))
+                if (string.IsNullOrWhiteSpace(input))
                 {
-                    // creates an array of strings that are split by commas
-                    string[] values = input.Split(',');
-
-                    // convert the array to int
-                    int[] myInts = Array.ConvertAll(values,int.Parse);
-
-                    // add the elements from the split array to the list of numbers
-                    myNums.AddRange(myInts);
-
-                    // checks if the array contains more than 5 numbers
-                    if (myNums.Count() >= 5)
-                    {
-                        // create a for loop that will take the 3 smallest numbers and insert them into the new list
-                        for (int i = 0; i < 3; i++)
-                        {
-                            int min = myNums.Min();
-
-                            // add the minimum value to the new list
-                            smallestNums.Add(min);
-
-                            // remove the minimum found from the original list so it will not be taken as a min again
-                            myNums.Remove(min);
-
-                        }
-
-                        // display the 3 smallest numbers
-                        Console.WriteLine($"The 3 smallest numbers are: {string.Join(", ", smallestNums)}");
-                        break;
-                    }
-
-                    // if the array contains less than 5 numbers prompts for a retry
-                    else
-                    {
-                        Console.WriteLine("Invalid list, try again!");
-                        continue;
-                    }
+                    Console.WriteLine("Empty list, try again!");
+                    continue;
                 }
 
-                // if the lists is empty prompt to try again
-                else
+                // split the input and try to parse it to an array of integers and then to a list
+                var numberList = input.Split(',').Select(n => {
+                    int result;
+                    if (int.TryParse(n.Trim(), out result)) return result;
+                    else return (int?)null;
+                }).Where(n => n.HasValue).Select(n => n.Value).ToList();
+
+                // if the array contains less than 5 numbers prompts for a retry
+                if (numberList.Count() < 5)
                 {
                     Console.WriteLine("Invalid list, try again!");
-                    continue;
+                }
+
+                else // checks if the array contains more than 5 numbers
+                {
+                    // get the 3 smallest numbers
+                    var smallestNumbers = numberList.OrderBy(n => n).Take(3);
+
+                    // display the 3 smallest numbers
+                    Console.WriteLine($"The 3 smallest numbers are: {string.Join(", ", smallestNumbers)}");
+                    break;
                 }
             }
         }
---
+++ Ex41/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter a few numbers separated by a hyphen.
+            //Work out if the numbers are consecutive.
+            //For example, if the input is "5-6-7-8-9" or "20-19-18-17-16",
+            //display a message: "Consecutive"; otherwise, display "Not Consecutive".
+
+
+        }
+    }
+}
