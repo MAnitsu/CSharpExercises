afaa04d55827147d0cead8d8b94732164806ed5b
MAnitsu <nitu.alexandru96@gmail.com>
2024-10-07 17:31:12 +0300

    added 2 solved exercises

---
+++ JustATest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEx1
+{
+    internal class Program
+    {
+        /* 
+         * Define a variable to hold your name
+         * Define a variable to hold your phone number
+         * Define a variable to hold your age
+         * Print variable line-by-line to the screen
+         * Extra: define variables using the var keyword
+         */
+        static void Main(string[] args)
+        {
+            var name = "Miai";
+            var phoneNumber = "0643593409";
+            var age = 27;
+            Console.WriteLine(name + "\n" + phoneNumber + "\n" + age);
+        }
+    }
+}
---
+++ LearningEx2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEx2
+{
+    internal class Program
+    {
+        /*
+         * Create and initialise two ints
+         * Make a variable to work out the remainder
+         * Output remainder to the screen
+         * Change the first int variable to another number
+             * and recalculate the remainder
+             * output new remainder to the screen
+         */
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = 2;
+            Console.WriteLine(a % b);
+
+            a = 20;
+            Console.WriteLine(a % b);
+        }
+    }
+}
