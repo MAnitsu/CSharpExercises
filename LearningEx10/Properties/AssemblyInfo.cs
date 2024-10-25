721e4f7bc0c5cab4a5e4190a66dc6f8c20225331
MAnitsu <nitu.alexandru96@gmail.com>
2024-10-25 10:21:34 +0300

    solved learning ex 9 and 10

---
+++ LearningEx10/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LearningEx10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LearningEx10")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de9b65a3-3b57-4c77-81eb-202ac282072a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LearningEx8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEx8
+{
+    /*
+     * Define and initialize two integers (num, length)
+     * (7, 5) -> [7, 14, 21, 28, 35]
+     * Create int array with size length
+     * loop through and insert the (loop counter x num) into the array
+     * print the final array */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 7;
+            int length = 5;
+            int[] result = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = (i + 1) * num;
+            }
+
+            foreach (int i in result)
+            {
+                Console.WriteLine($"{num} * {Array.IndexOf(result, i) + 1} = {i}");
+            }
+        }
+    }
+}
